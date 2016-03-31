5612e535681ec9cfcbd9d8a3dd06bea7955404af
Amrita974 <amrit.para@gmail.com>
2016-03-31 10:44:16 +0200

    Added files via upload
    Voila c'est pour les  parties des pages qui sont toujours pareils après il y a juste des modifications à faire  sur haut.php comme l'entete a legerement changé depuis. Et n'oubliez pas de prendre les fichiers .css associés et de modifier le chemin pour que ça fonctionne.

---
+++ bas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+     <meta charset="utf-8">
+     <link rel="Stylesheet" href="../style/bas.css" >
+</head>
+
+<body>
+<div id=footer>
+  <div class="Autres pages">
+      <h1>Autre page</h1>
+      <p><a href="Aide.html">Aide</a></br>
+      <a href="Forum.html">Forum</a></br>
+      <a href="Autre page.html">Autre page</a></p>
+  </div>
+    <div class="Réseaux sociaux">
+       <h1>Réseaux sociaux</h1>
+         <a href="https://www.facebook.com/Sportymates-1116430581702970/?ref=bookmarks"><img src="../images/logofacebook.jpg" alt="Logo Facebook" /></a>
+         <a href="https://twitter.com/sportymates?lang=fr"><img src="../images/logotwitter.png" alt="Logo twitter" /></a>
+         <a href="https://plus.google.com/"><img src="../images/logogoogleplus.png" alt="Logo google plus" /></a>
+    </div>
+    <div class="Copyright_et_mention_legales">
+     <h1>Copyright et mention légales</h1>
+     <p>© 2016  Sportymates</p>
+    </div>
+</div>
+</body>
+</html>
---
+++ haut.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="Stylesheet" href="../style/haut.css">
+</head>
+<body>
+<header>
+    <div id="titre_principal">
+        <div id="logo">
+           <figure>
+            <a href=accueil.html><img src="../images/logo3.png" alt="Logo de Sportymates"/></a>
+            <h1><figcaption><span style="color:red">S</span>portymates</figcaption></h1>
+           </figure>
+        </div>
+    </div>
+    <div id="inscription">
+      <a href="inscrire.html">S'inscrire </a>
+      <a href="connecter.html">Se connecter</a>
+      <i class="icon icon-rechercher"></i>
+      <input type='text' id='recherche' name='recherche' placeholder=Rechercher... size="20" maxlength="15" >
+
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">A propos |</a></li>
+            <li><a href="#">Sports |</a></li>
+            <li><a href="#">Nos groupes |</a></li>
+            <li><a href="commentaire.php">Nos clubs |</a></li>
+            <li><a href="#">Forum |</a></li>
+            <li><a href="#">Aide</a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
