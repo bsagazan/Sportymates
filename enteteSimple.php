2862d976d8ad6375634e10f824cf6f8510bb7c6f
NathalieW <nathalie.wang@isep.fr>
2016-05-23 11:40:56 +0200

    header pour creerGroupe

---
+++ enteteSimple.php
@@ -0,0 +1,24 @@
+<div id="Menu">
+    <div id="logo">
+        <a href=accueil.php><img src="../images/Logo1.png" alt="Logo_Sportymates"/></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">À propos</a></li>
+            <li><a href="Groupes.php">Groupes</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="../Clubs/clubs.php">Clubs</a></li>
+            <li><a href="#">Aide</a></li>
+            <li><a href="#">Forum</a></li>
+        </ul>
+    </nav> 
+    <form name="Recherche simple" id ="formRechSimple">
+        <input type="search" name="recherche" placeholder="Que recherchez vous ?" size="40"/>
+        <input type="submit" name="bouton" class="bouton" value="Rechercher"/>
+    </form> 
+
+    <div id="raccourciProfil" >
+        <img src="../images/profile.png" id="imageProfil"/>
+        <h1> Nathalie</h1>
+    </div>
+</div>
