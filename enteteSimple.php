09c5993583850f81199bd1716abb14a08e0e6d6b
NathalieW <nathalie.wang@isep.fr>
2016-05-23 16:25:18 +0200

    Update enteteSimple.php

--- enteteSimple.php
+++ enteteSimple.php
@@ -22,3 +22,119 @@
         <h1> Nathalie</h1>
     </div>
 </div>
+
+<!--
+
+#Menu {
+    padding: 10px;
+    white-space: nowrap;
+    background-color: rgba(70, 79, 82, 0.89);
+    display: flex;
+    justify-content: space-around;
+    box-shadow:0 3px 6px rgba(0, 0, 0, 0.29);
+}
+
+#logo img {
+    width: 130px;
+    margin-right: 1%;
+}
+
+form{
+    padding: 0.5%
+}
+
+nav{
+    padding: 0.5%;
+}
+
+nav ul {
+    list-style-type: none;
+    display: flex;
+}
+
+nav li {
+    margin-right: 15px;
+    display: inline-block;
+
+}
+
+nav a {
+    font-size: 1.3em;
+    color: #f8fada;
+    text-decoration: none;
+}
+
+nav a:hover
+{ 
+    border-bottom: 3px solid coral;
+}
+
+input[name="recherche"] {
+    border-style: solid;
+    border-width: 1px;
+    border-color: #dddddd;
+    padding: 5px 10px;
+    width: 45.5%;
+    box-sizing: border-box;
+    font-size: 12px;
+    font-weight: normal;
+    /*letter-spacing: 1px;*/
+    margin-right: 1em;
+    white-space: nowrap;
+    height: 2.8em;
+}
+
+
+.bouton {
+    background-color: #ffa44f;
+    border-style: outset;
+    border-width : 2px;
+    border-color:#ffa44f;
+    padding: 5px 10px;
+    box-sizing: border-box;
+    font-size:13px;
+    font-weight: normal;
+    letter-spacing: 2px;
+    margin-right: 10px;
+    text-transform: uppercase;
+    color: white;
+    white-space: nowrap;
+    height: 2.6em;
+
+}
+
+.bouton:hover{
+    background-color: white;
+    color: #ffa44f;
+    cursor: pointer;
+}
+
+#raccourciProfil{
+    background-color:#ffa44f;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    padding-top: .5%;
+    font-family: PT sans;
+    letter-spacing: 1px;
+    padding-right: 1%;
+
+}
+
+#raccourciProfil:hover{
+    background-color: white;
+    color:#ffa44f;
+}
+
+header h1{
+    font-size: 100%;
+    padding-top: 8%;
+    padding-left: 5%;
+}
+
+#imageProfil{
+    height: 40px;
+    margin-left: 8%;
+}
+-->
+
