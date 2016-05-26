2196f92a3fc5da12e4bab1b0b68ec3db4f7a001c
NathalieW <nathalie.wang@isep.fr>
2016-05-26 18:12:33 +0200

    upload 26/05 - ajout nbreMembre

--- modele.php
+++ modele.php
@@ -10,20 +10,26 @@ catch(Exception $e)
 
 $var1 = $_GET["groupe"];
 if (isset($var1)){
-    $rep = $bdd->prepare('SELECT * FROM groupe WHERE nomGroupe = ?');
-    $rep->execute(array($var1));
-    $don=$rep->fetch();
-    $titre = $don['nomGroupe'];
-    $nomSport = $don['nomSport'];
-    $niveau = $don['niveau'];
-    $description = $don['description'];
-    $imgGroupe = $don['imgGroupe'];
-    $nbreMax = $don['nbreMax'];
+    $reponse = $bdd->prepare('SELECT * FROM groupe WHERE nomGroupe = ?');
+    $reponse -> execute(array($var1));
+    $donnees = $reponse->fetch();
+    $titre = $donnees['nomGroupe'];
+    $nomSport = $donnees['nomSport'];
+    $niveau = $donnees['niveau'];
+    $description = $donnees['description'];
+    $imgGroupe = $donnees['imgGroupe'];
+    $nbreMax = $donnees['nbreMax'];   
+    
+    $req = $bdd->prepare('SELECT COUNT(nomUtilisateur) FROM faitpartide WHERE nomGroupe = ?');
+    $req -> execute(array($var1));
+    $don = $req->fetch();
+    $nbreMembre = $don['COUNT(nomUtilisateur)'];
     include("profil-groupe.php");
+    
 }
 else{
     echo "Erreur !";
 }
-
+$reponse->closeCursor();
 
 ?>
